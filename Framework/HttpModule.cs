ba0c313b817cb990cc852729486920166cf71b05
souing.net.company <souing.net@company.com>
2017-12-28 18:01:03 +0800

    2017-12-28 公共类库

---
+++ Framework/HttpModule.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HttpModuleDemo.Test
+{
+    public class MyHttpModule : IHttpModule
+    {
+
+        /// <summary>
+        /// 处置由实现 System.Web.IHttpModule 的模块使用的资源（内存除外）
+        /// </summary>
+        public void Dispose() { }
+
+        /// <summary>
+        /// 初始化模块，并使其为处理请求做好准备。
+        /// </summary>
+        /// <param name="context"></param>
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += context_BeginRequest;//在 ASP.NET 响应请求时作为 HTTP 执行管线链中的第一个事件发生。
+            context.EndRequest += context_EndRequest;    //在 ASP.NET 响应请求时作为 HTTP 执行管线链中的最后一个事件发生。
+        }
+
+        /// <summary>
+        /// 在 ASP.NET 响应请求时作为 HTTP 执行管线链中的最后一个事件发生。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void context_EndRequest(object sender, EventArgs e)
+        {
+            HttpApplication application = sender as HttpApplication;
+            HttpContext context = application.Context;
+            HttpRequest request = application.Request;
+            HttpResponse response = application.Response;
+
+            response.Write("context_EndRequest >> 在 ASP.NET 响应请求时作为 HTTP 执行管线链中的最后一个事件发生");
+        }
+
+        /// <summary>
+        /// 在 ASP.NET 响应请求时作为 HTTP 执行管线链中的第一个事件发生。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void context_BeginRequest(object sender, EventArgs e)
+        {
+            HttpApplication application = sender as HttpApplication;
+            HttpContext context = application.Context;
+            HttpRequest request = application.Request;
+            HttpResponse response = application.Response;
+
+            response.Write("context_BeginRequest >> 在 ASP.NET 响应请求时作为 HTTP 执行管线链中的第一个事件发生");
+        }
+    }
+}
---
+++ Framework/StringHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HttpModuleDemo.App_Code
+{
+    public static class StringHelper
+    {
+          #region 判断对象是否为空
+          
+          /// <summary>
+          /// 判断对象是否为空，为空返回true
+          /// </summary>
+          /// <param name="data">要验证的对象</param>
+          public static bool IsNullOrEmpty(this object data)
+          {
+              //如果为null
+              if (data == null)
+              {
+                  return true;
+              }
+   
+              //如果为""
+              if (data is string)
+              {
+                  if (string.IsNullOrEmpty(data.ToString().Trim()))
+                  {
+                      return true;
+                  }
+              }
+   
+              //如果为DBNull
+              if (data is DBNull)
+              {
+                  return true;
+              }
+   
+              //不为空
+              return false;
+          }
+
+          #endregion
+    }
+}
