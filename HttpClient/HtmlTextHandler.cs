d670c6fdaf7395f52f165d110e30a9b80c4d457a
souing.net.company <souing.net@company.com>
2018-01-04 14:23:43 +0800

    2018-01-04 httphandler

---
+++ HttpClient/HtmlTextHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpClientTimeOut
+{
+    class HtmlTextHandler : HttpClientHandler
+    {
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = await base.SendAsync(request, cancellationToken);
+
+            var contentType = response.Content.Headers.ContentType;
+            contentType.CharSet = await getCharSetAsync(response.Content);
+
+            return response;
+        }
+
+        private async Task<string> getCharSetAsync(HttpContent httpContent)
+        {
+            var charset = httpContent.Headers.ContentType.CharSet;
+            if (!string.IsNullOrEmpty(charset))
+                return charset;
+
+            var content = await httpContent.ReadAsStringAsync();
+            var match = Regex.Match(content, @"charset=(?<charset>.+?)""", RegexOptions.IgnoreCase);
+            if (!match.Success)
+                return charset;
+
+            return match.Groups["charset"].Value;
+        }
+    }
+}
